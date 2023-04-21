817e1234cbb210adba807e282bbef01bc9bbe697
TakuOgawa <ogawtaku@gmail.com>
2023-04-21 10:26:20 +0900

    StatefulWidgetの状態管理

--- lib/main.dart
+++ lib/main.dart
@@ -47,21 +47,54 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const WidgetA(),
+            WidgetB(_counter),
+            WidgetC(_incrementCounter),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class WidgetA extends StatelessWidget {
+  const WidgetA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    print('WidgetA build() called');
+    return const Text(
+      'You have pushed the button this many times:',
+    );
+  }
+}
+
+class WidgetB extends StatelessWidget {
+  const WidgetB(this.counter, {super.key});
+  final int counter;
+
+  @override
+  Widget build(BuildContext context) {
+    print('WidgetB build() called');
+    return Text(
+      '$counter',
+      style: Theme.of(context).textTheme.headlineMedium,
+    );
+  }
+}
+
+class WidgetC extends StatelessWidget {
+  const WidgetC(this.increment, {super.key});
+  final Function increment;
+
+  @override
+  Widget build(BuildContext context) {
+    print('WidgetC build() called');
+    return ElevatedButton(
+      onPressed: () {
+        increment();
+      },
+      child: const Text('カウント'),
     );
   }
 }
